e4309558e026be13817537293127964c333058f7
qwnick <Mykola Davydenko>
2026-08-16 23:49:19 +0200

    in progress

---
+++ Assets/Scripts/EnemyStamina.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStamina : MonoBehaviour
+{
+	[Header("Stamina Settings")]
+	public float maxStamina = 100f;
+	public float currentStamina;
+
+	[Header("UI References")]
+	public Image staminaBarFill; // Drag your UI Image here
+
+
+	void Start()
+	{
+		// Initialize stamina to maximum at the start
+		currentStamina = maxStamina;
+		UpdateStaminaUI();
+	}
+
+	void Update()
+	{
+		UpdateStaminaUI();
+	}
+	private void UpdateStaminaUI()
+	{
+		if (staminaBarFill != null)
+		{
+			// The fillAmount expects a value between 0 and 1
+			staminaBarFill.fillAmount = currentStamina / maxStamina;
+		}
+	}
+}
